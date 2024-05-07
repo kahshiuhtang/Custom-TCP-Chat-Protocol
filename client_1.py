2a7285f040513379ea07e297d97acb67e92822a5
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-05-06 23:21:22 -0400

    Finished part 1

---
+++ client_1.py
@@ -0,0 +1,191 @@
+'''
+This module defines the behaviour of a client in your Chat Application
+'''
+import sys
+import getopt
+import socket
+import random
+from threading import Thread
+import os
+import util
+import time
+import logging
+
+
+'''
+Write your code inside this class. 
+In the start() function, you will read user-input and act accordingly.
+receive_handler() function is running another thread and you have to listen 
+for incoming messages in this function.
+'''
+
+class Client:
+    '''
+    This is the main Client Class. 
+    '''
+    def __init__(self, username, dest, port, window_size):
+        self.server_addr = dest
+        self.server_port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.settimeout(None)
+        self.sock.bind(('', random.randint(10000, 40000)))
+        self.username = username
+        self.window_size = window_size
+        self.logger = logging.getLogger(__name__)
+        logging.basicConfig(filename='./logs/client_' + str(username) +'.log', encoding='utf-8', level=logging.DEBUG)
+
+    def start(self):
+        '''
+        Main Loop is here
+        Start by sending the server a JOIN message. 
+        Use make_message() and make_util() functions from util.py to make your first join packet
+        Waits for userinput and then process it
+        '''
+        join_msg = util.make_message("join", 1, self.username)
+        pack = util.make_packet(msg=join_msg)
+        self.sock.sendto(pack.encode('utf-8'), (self.server_addr, self.server_port))
+        while True:
+            message = input("")
+            if message.lower() == 'quit':
+                self.logger.debug('[INPUT_MSG]: Quit')
+                self.exit_client()
+                break
+            input_words = message.split()
+            cmd = input_words[0].lower()
+
+            if cmd == "msg":
+                self.logger.debug('[INPUT_MSG]: Msg')
+                msg_content = input_words[1] + " "
+                for entry_count in range(0, int(input_words[1])):
+                    msg_content += input_words[2 + entry_count] + " "
+                actual_msg = " ".join(input_words[int(input_words[1]) + 2:])
+                msg_content = msg_content + actual_msg
+                send_msg = util.make_message("send_message", 4, msg_content)
+                pack = util.make_packet(msg=send_msg)
+                self.sock.sendto(pack.encode('utf-8'), (self.server_addr, self.server_port))
+            elif cmd == "list":
+                self.logger.debug('[INPUT_MSG]: List')
+                list_msg = util.make_message("request_users_list", 2)
+                pack = util.make_packet(msg=list_msg)
+                self.sock.sendto(pack.encode('utf-8'), (self.server_addr, self.server_port))
+            elif cmd == "help":
+                self.logger.debug('[INPUT_MSG]: Help')
+                self.print_help()
+            else:
+                self.logger.debug('[INPUT_MSG]: Unknown message')
+                print("incorrect userinput format")
+                pass
+            
+
+    def receive_handler(self):
+        '''
+        Waits for a message from server and process it accordingly
+        '''
+        try:
+            while True:
+                data, client_address = self.sock.recvfrom(1024)
+                self.logger.debug('[RECV_MSG]: packet')
+                decoded_data = data.decode('utf-8')
+                segments = decoded_data.split("|")
+                self.logger.debug(segments)
+                msg = segments[2].split()
+                if msg[0] == "response_users_list":
+                    self.logger.debug('[RECV_MSG]: response_users_list')
+                    sent_message_whole = segments[2].split()
+                    comb_msg = " ".join(sent_message_whole[3:])
+                    print("list: " + comb_msg)
+                elif msg[0] == "forward_message":
+                    self.logger.debug('[RECV_MSG]: forward_message')
+                    sent_message_whole = segments[2].split()
+                    sender = sent_message_whole[3]
+                    comb_msg = " ".join(sent_message_whole[4:])
+                    print("msg: " + sender +": " + comb_msg)
+                elif msg[0] == "err_unknown_message":
+                    self.logger.debug('[RECV_MSG]: err_unknown_message')
+                    print("disconnected: server received an unknown command")
+                    self.exit_client()
+                    return
+                elif msg[0] == "err_server_full":
+                    self.logger.debug('[RECV_MSG]: err_server_full')
+                    print("disconnected: server full")
+                    self.exit_client()
+                    return
+                elif msg[0] == "err_username_unavailable":
+                    self.logger.debug('[RECV_MSG]: err_username_unavailable')
+                    print("disconnected: username not available")
+                    self.exit_client()
+                    return
+                else:
+                    self.logger.debug('[RECV_MSG]: Unknown message sent')
+        except:
+              self.logger.debug("[SERVER]: Timeout reached. Moving on.")
+
+    def exit_client(self):
+        disconnect_msg = util.make_message("disconnect", 1, self.username)
+        pack = util.make_packet(msg=disconnect_msg)
+        self.sock.sendto(pack.encode('utf-8'), (self.server_addr, self.server_port))
+        self.logger.debug("[SERVER]: Just sent disconnect packet, will it make it")
+        time.sleep(0.5)
+        print("quitting")
+
+    def print_help(self):
+        '''
+        This function is just for the sake of our Client module completion
+        '''
+        print("Client")
+        print("-u username | --user=username The username of Client")
+        print("-p PORT | --port=PORT The server port, defaults to 15000")
+        print("-a ADDRESS | --address=ADDRESS The server ip or hostname, defaults to localhost")
+        print("-w WINDOW_SIZE | --window=WINDOW_SIZE The window_size, defaults to 3")
+        print("-h | --help Print this help")
+
+
+
+# Do not change below part of code
+if __name__ == "__main__":
+    def helper():
+        '''
+        This function is just for the sake of our Client module completion
+        '''
+        print("Client")
+        print("-u username | --user=username The username of Client")
+        print("-p PORT | --port=PORT The server port, defaults to 15000")
+        print("-a ADDRESS | --address=ADDRESS The server ip or hostname, defaults to localhost")
+        print("-w WINDOW_SIZE | --window=WINDOW_SIZE The window_size, defaults to 3")
+        print("-h | --help Print this help")
+    try:
+        OPTS, ARGS = getopt.getopt(sys.argv[1:],
+                                   "u:p:a:w", ["user=", "port=", "address=","window="])
+    except getopt.error:
+        helper()
+        exit(1)
+
+    PORT = 15000
+    DEST = "localhost"
+    USER_NAME = None
+    WINDOW_SIZE = 3
+    for o, a in OPTS:
+        if o in ("-u", "--user="):
+            USER_NAME = a
+        elif o in ("-p", "--port="):
+            PORT = int(a)
+        elif o in ("-a", "--address="):
+            DEST = a
+        elif o in ("-w", "--window="):
+            WINDOW_SIZE = a
+
+    if USER_NAME is None:
+        print("Missing Username.")
+        helper()
+        exit(1)
+
+    S = Client(USER_NAME, DEST, PORT, WINDOW_SIZE)
+    try:
+        # Start receiving Messages
+        T = Thread(target=S.receive_handler)
+        T.daemon = True
+        T.start()
+        # Start Client
+        S.start()
+    except (KeyboardInterrupt, SystemExit):
+        sys.exit()
---
+++ server_1.py
@@ -0,0 +1,187 @@
+'''
+This module defines the behaviour of server in your Chat Application
+'''
+import sys
+import getopt
+import socket
+import util
+import logging
+import threading
+import time
+
+
+class Server:
+    '''
+    This is the main Server Class. You will  write Server code inside this class.
+    '''
+    def __init__(self, dest, port, window):
+        self.server_addr = dest
+        self.server_port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.settimeout(None)
+        self.sock.bind((self.server_addr, self.server_port))
+        self.usernames = dict()
+        self.window = window
+        self.logger = logging.getLogger(__name__)
+        logging.basicConfig(filename='./logs/server.log', encoding='utf-8', level=logging.DEBUG)
+
+    def start(self):
+        '''
+        Main loop.
+        continue receiving messages from Clients and processing it.
+
+        '''
+        self.logger.debug('Starting Server')
+        try:
+            while True:
+                self.logger.debug('[SERVER]: Waiting for new packet')
+                data, client_address = self.sock.recvfrom(1024)
+                decoded_data = data.decode('utf-8')
+                segments = decoded_data.split("|")
+                self.logger.debug('[SERVER]: Recieved packet:')
+                self.logger.debug(segments)
+                self.logger.debug("FROM: ")
+                self.logger.debug(client_address)
+                if len(segments) < 3:
+                    self.logger.debug('[ERROR]: Wrong sized packet')
+                    continue
+                msg = segments[2].split()
+                if msg[0] == "join":
+                    self.logger.debug('[MSG]: Join')
+                    if len(msg) < 3:
+                        self.logger.debug('[ERROR]: Join messsage has less than 3 items')
+                        continue
+                    name = msg[2]
+                    if len(self.usernames) == util.MAX_NUM_CLIENTS:
+                        self.logger.debug('[SERVER]: Max clients hit in JOIN')
+                        full_serv_msg = util.make_message(msg_type="err_server_full", msg_format=2)
+                        pkt = util.make_packet(msg=full_serv_msg)
+                        self.sock.sendto(pkt.encode('utf-8'), (client_address[0], client_address[1]))
+                        continue
+                    if name in self.usernames.keys():
+                        self.logger.debug('[SERVER]: Name found in usernames')
+                        used_msg = util.make_message(msg_type="err_username_unavailable", msg_format=2)
+                        pkt = util.make_packet(msg=used_msg)
+                        self.sock.sendto(pkt.encode('utf-8'), (client_address[0], client_address[1]))
+                    else:
+                        self.logger.debug('Adding this username to list of usernames')
+                        self.usernames.update({name: client_address})
+                        print("join: " + str(name))
+                elif msg[0] == "request_users_list":
+                    self.logger.debug('[MSG]: Request Users List')
+                    num_users = len(self.usernames)
+                    user_string =  ""
+                    for name in sorted(self.usernames.keys()):
+                        user_string += name + " "
+                    if user_string != "":
+                        user_string = user_string[:len(user_string) - 1]
+                    user_string = str(num_users) + " " + user_string
+                    users_msg = util.make_message(msg_type="response_users_list", msg_format=3, message=user_string)
+                    pkt = util.make_packet(msg=users_msg)
+                    self.logger.debug(pkt)
+                    self.sock.sendto(pkt.encode('utf-8'), (client_address[0], client_address[1]))
+                    username = self.get_username(client_address=client_address)
+                    print("request_users_list: " + str(username))
+                elif msg[0] == "send_message":
+                    self.logger.debug('[MSG]: Send Message')
+                    num_recipients = int(msg[2])
+                    recipients = msg[3: 3 + num_recipients]
+                    sent_to = set()
+                    msg_to_send = " ".join(msg[3 + num_recipients :])
+                    self.logger.debug('[Server]: Msg To Send')
+                    self.logger.debug(msg_to_send)
+                    self.logger.debug('[Server]: Recipients')
+                    self.logger.debug(recipients)
+                    sender = self.get_username(client_address=client_address)
+                    print("msg: " + str(sender))
+                    for idx in range(0, num_recipients):
+                        user = recipients[idx]
+                        if user in sent_to:
+                            self.logger.debug('[Server]: Duplicate address specified, ' + str(user))
+                            pass
+                        else:
+                            sent_to.add(user)
+                            if user not in self.usernames.keys():
+                                print("msg: " + str(sender) + " to non-existent user " + user)
+                            else:
+                                address, port = self.usernames[user]
+                                msg_content = "1 " + sender + " " + msg_to_send
+                                send_msg_user = util.make_message(msg_type="forward_message", msg_format=4, message=msg_content)
+                                pkt = util.make_packet(msg=send_msg_user)
+                                self.logger.debug(pkt)
+                                self.logger.debug(address)
+                                self.logger.debug(port)
+                                self.sock.sendto(pkt.encode('utf-8'), (address, port))
+                elif msg[0] == "disconnect":
+                    self.logger.debug('[MSG]: Disconnect')
+                    if len(msg) < 3:
+                        self.logger.debug('[ERROR]: Invalid message content for disconnect')
+                        continue
+                    name = msg[2]
+                    self.handle_disconnect(name)
+                else:
+                    self.logger.debug('[MSG]: Unknown Message')
+                    used_msg = util.make_message(msg_type="err_unknown_message", msg_format=2)
+                    pkt = util.make_packet(msg=used_msg)
+                    self.sock.sendto(pkt.encode('utf-8'), (client_address[0], client_address[1]))
+                    username = self.get_username(client_address=client_address)
+                    self.handle_disconnect(username)
+                    print("disconnected: " + username+ " sent unknown command")
+        except Exception as e:
+            self.logger.debug("[SERVER]: Ending server due to exception.")
+            self.logger.debug(e)
+            self.sock.close()
+
+    def handle_disconnect(self, name):
+        self.logger.debug("[SERVER]: Handling disconnect for user " + name)
+        if name in self.usernames.keys():
+            del self.usernames[name]
+        else:
+            self.logger.debug("[SERVER]: Error, unable to disconnect this user")
+        print("disconnected: " + str(name))
+
+    def get_username(self, client_address):
+        for username in self.usernames.keys():
+            address, port = self.usernames[username]
+            if address == client_address[0] and port == client_address[1]:
+                return username
+        return ""
+
+# Do not change below part of code
+
+if __name__ == "__main__":
+    def helper():
+        '''
+        This function is just for the sake of our module completion
+        '''
+        print("Server")
+        print("-p PORT | --port=PORT The server port, defaults to 15000")
+        print("-a ADDRESS | --address=ADDRESS The server ip or hostname, defaults to localhost")
+        print("-w WINDOW | --window=WINDOW The window size, default is 3")
+        print("-h | --help Print this help")
+
+    try:
+        OPTS, ARGS = getopt.getopt(sys.argv[1:],
+                                   "p:a:w", ["port=", "address=","window="])
+    except getopt.GetoptError:
+        helper()
+        exit()
+
+    PORT = 15000
+    DEST = "localhost"
+    WINDOW = 3
+
+    for o, a in OPTS:
+        if o in ("-p", "--port="):
+            PORT = int(a)
+        elif o in ("-a", "--address="):
+            DEST = a
+        elif o in ("-w", "--window="):
+            WINDOW = a
+
+    SERVER = Server(DEST, PORT,WINDOW)
+    try:
+        SERVER.start()
+    except (KeyboardInterrupt, SystemExit):
+        exit()
